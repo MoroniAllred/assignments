816602e5cfd8c65399bafaff413df15037515039
Moroni Allred <moroniallred@gmail.com>
2020-01-28 18:42:24 -0700

    added vanity

--- projects/portfolio/src/components/Projects.js
+++ projects/portfolio/src/components/Projects.js
@@ -7,8 +7,9 @@ const Project = () => {
             <h1>Projects</h1>
             <p>Click on a Image to visit the webpage.</p>
             <div className="parentProjectPic">
-                <a href="http://laugh-o-l.surge.sh/" target="_blank"><div className="projectPic" id="laughOL"></div></a>  
+                <a href="https://laugh-o-l.surge.sh/" target="_blank"><div className="projectPic" id="laughOL"></div></a>  
                 <a href="https://fullstackpizza.herokuapp.com/" target="_blank"><div className="projectPic" id="fullStackPizza"></div></a>
+                <a href="https://vanity-parlor.herokuapp.com/" target="_blank"><div className="projectPic" id="vanity"></div></a>
             </div>
         </div>
     )
